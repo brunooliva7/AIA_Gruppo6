9795dd1a83d52839f739e08bc7498d6a613eac80
brux1o <bruno.oliva2004@gmail.com>
2026-06-18 15:31:30 +0200

    p

--- NavigazioneAssistita.py
+++ NavigazioneAssistita.py
@@ -40,12 +40,8 @@ TRADUZIONI_ITA = {
     'umbrella': 'un ombrello', 'suitcase': 'una valigia'
 }
 
-BLU_LOWER = np.array([95, 50, 50])
+BLU_LOWER = np.array([95, 70, 30])
 BLU_UPPER = np.array([135, 255, 255])
-
-# Soglia V minima: pixel con V sotto questa soglia sono troppo scuri
-# per essere linea (probabilmente ombra). Tagliati prima della maschera blu.
-V_MIN_LINEA = 60
 
 SOGLIA_MINIMA_PIXEL       = 3000
 SOGLIA_Y_INCROCIO         = 320
@@ -338,23 +334,9 @@ class AnalizzatoreLinea:
 
         blur = cv2.GaussianBlur(frame, (5, 5), 0)
         hsv  = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
-
-        # Maschera blu (range ampliato per tollerare riflessi a saturazione bassa)
         mask = cv2.inRange(hsv, blu_lower, blu_upper)
-
-        # Esclusione ombre: rimuove i pixel troppo scuri (V < soglia) che cadono
-        # nel range blu solo perché ombra su pavimento bluastro.
-        V = hsv[:, :, 2]
-        mask_no_ombra = cv2.threshold(V, V_MIN_LINEA, 255, cv2.THRESH_BINARY)[1]
-        mask = cv2.bitwise_and(mask, mask_no_ombra)
-
-        # OPEN piccolo: rimuove rumore puntuale
-        kernel_open = np.ones((5, 5), np.uint8)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel_open)
-
-        # CLOSE: ricuce i buchi della linea dovuti a riflessi
-        kernel_close = np.ones((15, 15), np.uint8)
-        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel_close)
+        kernel = np.ones((5, 5), np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
 
         contorni, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         contorni_validi = [c for c in contorni if cv2.contourArea(c) > 1500]
